d1b5142e126b9a47396c76c95a88d4812e8a7f8b
Akasha0510 <121181538+Akasha0510@users.noreply.github.com>
2024-09-09 22:13:52 +0530

    app.js

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ function takeCommand(message) {
     } else if (message.includes("open facebook")) {
         window.open("https://facebook.com", "_blank");
         speak("Opening Facebook...");
+    } else if (message.includes("open instagram")) {
+        window.open("https://www.instagram.com", "_blank");
+        speak("Opening Instagram...");     
     } else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "This is what I found on the internet regarding " + message;
